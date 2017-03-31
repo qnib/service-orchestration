67594883ea384469f394249ca0cc107bf4aacd54
Christian Kniep <christian@qnib.org>
2017-03-31 17:47:54 +0200

    rename to predeploy and use generic stack_deploy.sh

---
+++ influxdb/predeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATA_DIR=${DATA_DIR:-/var/lib/influxdb/}
+
+echo "[II] Ensure data dir '${DATA_DIR}/{grafana/sql,influxdb}' is present"
+# Idempotent, as it uses 'mkdir -p'. Even though it mounts the root of the server it can 'only' create arbitrary directories.
+export DATA_DIR
+docker run -t --rm -v /:/root_dir/ alpine mkdir -p /root_dir/${DATA_DIR}/{grafana/sql,influxdb}
+
