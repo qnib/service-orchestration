5211b4961ceb20ff1ba3733838aeff1fc59d2e2d
Christian Kniep <christian@qnib.org>
2017-03-31 16:21:23 +0200

    WIP: update deploy script

--- influxdb/backup_grafana.sh
+++ influxdb/backup_grafana.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];then
-   echo ">> ./backup.sh dashboard|alert <name>"
-   exit 1
-fi
-NODE=$(docker service ps -f desired-state=running influx_frontend | awk '/_frontend/{print $4}')
-ADDR=$(docker node inspect -f '{{ .Status.Addr }}' ${NODE})
-if [ "${NODE}" == "moby" ];then
-    DOCKER_HOST=unix:///var/run/docker.sock
-else
-    DOCKER_HOST=${ADDR}:2376
-fi
-CNT_NAME=$(docker ps | awk '/_frontend/{print $NF}')
-docker exec -t ${CNT_NAME} sqlite3 /var/lib/grafana/grafana.db .dump | egrep "INSERT INTO \"${1}\".*${2}"
+SERVICE=$(basename "$(pwd)")
+CNT=$(docker ps -f label=com.docker.swarm.service.name=${SERVICE}_frontend -q)
 
+set -x
+docker exec -t ${CNT} rm -f /opt/grafana/sql/dashboards/*
+docker exec -t ${CNT} /opt/qnib/grafana/bin/backup_dash.sh /opt/grafana/sql/dashboards/
--- influxdb/deploy.sh
+++ influxdb/deploy.sh
@@ -3,9 +3,12 @@
 DATA_DIR=${DATA_DIR:-/var/lib/influxdb/}
 SERVICE=$(basename "$(pwd)")
 
-echo "> Ensure data dir '${DATA_DIR}' is present"
+/opt/service-scripts/gocd/common/bin/download-artifacts.sh
+IMG_NAME=$(find ./target/ -name "*.image_name" |head -n1)
+
+echo "> Ensure data dir '${DATA_DIR}/{grafana/sql,influxdb}' is present"
 # Idempotent, as it uses 'mkdir -p'. Even though it mounts the root of the server it can 'only' create arbitrary directories.
 export DATA_DIR
-docker run -t --rm -v /:/root_dir/ alpine mkdir -p /root_dir/${DATA_DIR}
+docker run -t --rm -v /:/root_dir/ alpine mkdir -p /root_dir/${DATA_DIR}/{grafana/sql,influxdb}
 echo "> Deploy stack from docker-compose file"
 docker stack deploy --compose-file docker-compose.yml ${SERVICE}
