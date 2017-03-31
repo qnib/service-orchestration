ecc931917bb7b87ffe291daa7a10896fe691de59
Christian Kniep <christian@qnib.org>
2017-03-31 10:13:52 +0200

    adjust deploy script

--- influxdb/deploy.sh
+++ influxdb/deploy.sh
@@ -5,6 +5,7 @@ SERVICE=$(basename "$(pwd)")
 
 echo "> Ensure data dir '${DATA_DIR}' is present"
 # Idempotent, as it uses 'mkdir -p'. Even though it mounts the root of the server it can 'only' create arbitrary directories.
+export DATA_DIR
 docker run -t --rm -v /:/root_dir/ alpine mkdir -p /root_dir/${DATA_DIR}
 echo "> Deploy stack from docker-compose file"
 docker stack deploy --compose-file docker-compose.yml ${SERVICE}
